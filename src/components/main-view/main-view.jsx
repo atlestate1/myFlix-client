f35c3f8e539500e642a4cc9adedf982a63257676
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2022-06-05 12:01:03 -0400

    minor updates
    coding updates

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -82,3 +82,5 @@ export class MainView extends React.Component {
     }
 
 }
+
+MainView.propTypes = {};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -47,3 +47,20 @@ export class MovieView extends React.Component {
         );
     }
 }
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string,
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string,
+            Bio: PropTypes.string,
+            Birthday: PropTypes.string,
+        }),
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
